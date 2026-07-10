723dcf608366592f8bcbe7d37db40c1f442db1dd
Rodrigo Vieira <rodrigodenizads@gmail.com>
2026-07-09 22:03:07 -0300

    test: configure jest and react testing library

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Path to the Next.js app to load next.config and .env files in the test environment
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/mocks/**',
+  ],
+};
+
+export default createJestConfig(config);
